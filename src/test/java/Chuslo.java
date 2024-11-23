49e5a3691258a9b5199a3f6759afb3eef71b088c
JuliaOshmarina <ju99478@gmail.com>
2024-11-23 19:19:07 +0500

    Update Chuslo.java

--- src/test/java/Chuslo.java
+++ src/test/java/Chuslo.java
@@ -1,11 +1,14 @@
 
 public class Chuslo {
     public static void main(String[] args) {
+
+        private static Int msg = b;
+        
         int a = 5;
         int b = 10;
         if (a > b)
             System.out.println(a + "больше");
         else
-            System.out.println(b);
+            System.out.println(msg);
     }
 }
